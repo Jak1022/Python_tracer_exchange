7c0fd89ef82e806177ebc6cbee283405bc0bd660
Jak1022 <70177860+Jak1022@users.noreply.github.com>
2024-07-29 20:59:03 -0400

    upload code

--- code/Python_tracer_exchange.py
+++ code/Python_tracer_exchange.py
@@ -9,10 +9,8 @@ times = 1800
 
 with open('tracer_exchange_simulation.txt', 'w') as f:
     f.write('Time 6Li_abundance_in_electrode 6Li_abundance_in_electrolyte\n')
-
     for i in range(times + 1):
         x2 = (m2 * x2 + n * x1) / (m2 + n)
         x1 = (n * x2 + (m1 - n) * x1) / m1
         f.write(f'{i} {x1:.4f} {x2:.4f}\n')
-        
         print(f'{i} {x1:.4f} {x2:.4f}')
