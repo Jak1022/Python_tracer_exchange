6dcb8786e6f2ca0db7ff373f55e9bf01cad5700b
Jak1022 <70177860+Jak1022@users.noreply.github.com>
2024-07-30 13:51:14 -0400

    Update Python_tracer_exchange.py

--- code/Python_tracer_exchange.py
+++ code/Python_tracer_exchange.py
@@ -1,11 +1,14 @@
 # Python code for simulating tracer exchange
+import time
 
 m1 = 100
 x1 = 0.95
 m2 = 300.47
 x2 = 0.069825
 n = 0.1088
-times = 1800
+times = 1800 # iteration
+
+start_time = time.time()
 
 with open('tracer_exchange_simulation.txt', 'w') as f:
     f.write('Time 6Li_abundance_in_electrode 6Li_abundance_in_electrolyte\n')
@@ -14,3 +17,7 @@ with open('tracer_exchange_simulation.txt', 'w') as f:
         x1 = (n * x2 + (m1 - n) * x1) / m1
         f.write(f'{i} {x1:.4f} {x2:.4f}\n')
         print(f'{i} {x1:.4f} {x2:.4f}')
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f'Simulation run time: {elapsed_time:.2f} seconds')
